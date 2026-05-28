ef64b890af57c72eb7481936e17a86964277b925
robertozv80 <robzambra@hotmail.com>
2026-05-28 14:01:56 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
+        classpath("com.android.tools.build:gradle:9.1.1")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
@@ -18,7 +19,6 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "com.android.library")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     dependencies {
