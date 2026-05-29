c7d2f4a5bbdb6d5e4dadc53dfeb85b5c6586c854
robertozv80 <robzambra@hotmail.com>
2026-05-28 23:38:46 -0500

    Update build.gradle.kts

--- DonghuaLifeProvider/build.gradle.kts
+++ DonghuaLifeProvider/build.gradle.kts
@@ -13,5 +13,6 @@ cloudstream {
 
 dependencies {
     add("cloudstream", "com.lagradost:cloudstream3:pre-release")
+    add("implementation", "com.github.Blatzar:NiceHttp:0.4.11")
     add("implementation", "org.jsoup:jsoup:1.18.3")
 }
