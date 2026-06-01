0fbfdc136342bf40b537e81cf1d6031b4e54f608
robertozv80 <robzambra@hotmail.com>
2026-05-31 22:37:36 -0500

    Update AnimeGratisProviderPlugin.kt

--- AnimeGratisProvider/src/main/kotlin/com/donghuaext/AnimeGratisProviderPlugin.kt
+++ AnimeGratisProvider/src/main/kotlin/com/donghuaext/AnimeGratisProviderPlugin.kt
@@ -1,10 +1,10 @@
 package com.donghuaext
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnimeGratisProviderPlugin : Plugin() {
+class AnimeGratisProviderPlugin : BasePlugin() {
     override fun load() {
         registerMainAPI(AnimeGratisProvider())
     }
