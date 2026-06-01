4beba67cb8a38220132b606a66a826130c8c6cc2
robertozv80 <robzambra@hotmail.com>
2026-06-01 01:01:25 -0500

    Update DonghuaLifeProviderPlugin.kt

--- DonghuaLifeProvider/src/main/kotlin/com/donghuaext/DonghuaLifeProviderPlugin.kt
+++ DonghuaLifeProvider/src/main/kotlin/com/donghuaext/DonghuaLifeProviderPlugin.kt
@@ -1,10 +1,10 @@
 package com.donghuaext
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class DonghuaLifeProviderPlugin : BasePlugin() {
+class DonghuaLifeProviderPlugin : Plugin() {
     override fun load() {
         registerMainAPI(DonghuaLifeProvider())
     }
