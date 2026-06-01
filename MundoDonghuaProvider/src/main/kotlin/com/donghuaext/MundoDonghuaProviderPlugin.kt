73795f3804b7f3ef60e81ed78b341b1e432e5a5a
robertozv80 <robzambra@hotmail.com>
2026-05-31 22:36:10 -0500

    Update MundoDonghuaProviderPlugin.kt

--- MundoDonghuaProvider/src/main/kotlin/com/donghuaext/MundoDonghuaProviderPlugin.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/donghuaext/MundoDonghuaProviderPlugin.kt
@@ -1,10 +1,10 @@
 package com.donghuaext
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class MundoDonghuaProviderPlugin : Plugin() {
+class MundoDonghuaProviderPlugin : BasePlugin() {
     override fun load() {
         registerMainAPI(MundoDonghuaProvider())
     }
