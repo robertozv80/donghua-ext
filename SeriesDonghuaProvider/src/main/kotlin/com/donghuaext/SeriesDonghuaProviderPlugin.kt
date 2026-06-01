99148bd07054e072d5af887f37dfd4c3b1de02a1
robertozv80 <robzambra@hotmail.com>
2026-06-01 01:03:06 -0500

    Update SeriesDonghuaProviderPlugin.kt

--- SeriesDonghuaProvider/src/main/kotlin/com/donghuaext/SeriesDonghuaProviderPlugin.kt
+++ SeriesDonghuaProvider/src/main/kotlin/com/donghuaext/SeriesDonghuaProviderPlugin.kt
@@ -1,10 +1,10 @@
 package com.donghuaext
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class SeriesDonghuaProviderPlugin : BasePlugin() {
+class SeriesDonghuaProviderPlugin : Plugin() {
     override fun load() {
         registerMainAPI(SeriesDonghuaProvider())
     }
